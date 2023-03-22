417648f91abe3f336874b2a22e7ebc32bd1fc92d
root <kealebogamoshupa@live.com>
2023-03-22 13:22:09 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+  1 #include "main.h"
+  2 /**
+  3  * print_alphabet - Prints the alphabet in lowercase
+  4  *
+  5  * Return: Always 0 (Success)
+  6  */
+  7 void print_alphabet(void)
+  8 {
+  9	  int i;
+ 10
+ 11	 fot (i = 97; i <= 122; i++)
+ 12	 {
+ 13		 _putchar(i);
+ 14	 }
+ 15      _putchar('\n');
+ 16 } 
